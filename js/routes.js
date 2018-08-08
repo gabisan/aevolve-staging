0c8af5f19a57a7103eb78db052e06f08166f498b
Charlon Jeff Cortez <cj@sportspass.com.au>
2018-08-08 21:34:53 +0800

    convert html to angularjs

---
+++ js/routes.js
@@ -0,0 +1,139 @@
+angular
+.module('app')
+.config(['$stateProvider', '$urlRouterProvider', '$ocLazyLoadProvider', '$breadcrumbProvider', function($stateProvider, $urlRouterProvider, $ocLazyLoadProvider, $breadcrumbProvider) {
+
+  $urlRouterProvider.otherwise('/dashboard');
+
+  $ocLazyLoadProvider.config({
+    // Set to true if you want to see what and when is dynamically loaded
+    debug: true
+  });
+
+  $breadcrumbProvider.setOptions({
+    prefixStateName: 'app.main',
+    includeAbstract: true,
+    template: '<li class="breadcrumb-item" ng-repeat="step in steps" ng-class="{active: $last}" ng-switch="$last || !!step.abstract"><a ng-switch-when="false" href="{{step.ncyBreadcrumbLink}}">{{step.ncyBreadcrumbLabel}}</a><span ng-switch-when="true">{{step.ncyBreadcrumbLabel}}</span></li>'
+  });
+
+  $stateProvider
+  .state('app', {
+    abstract: true,
+    templateUrl: 'views/common/layouts/full.html',
+    //page title goes here
+    ncyBreadcrumb: {
+      label: 'Root',
+      skip: true
+    },
+    resolve: {
+      loadCSS: ['$ocLazyLoad', function($ocLazyLoad) {
+        // you can lazy load CSS files
+        return $ocLazyLoad.load([{
+          serie: true,
+          name: 'Font Awesome',
+          files: ['css/fontawesome-all.css']
+        },{
+          serie: true,
+          name: 'Simple Line Icons',
+          files: ['css/simple-line-icons.css']
+        }]);
+      }],
+      loadPlugin: ['$ocLazyLoad', function ($ocLazyLoad) {
+        // you can lazy load files for an existing module
+        return $ocLazyLoad.load([{
+          serie: true,
+          name: 'chart.js',
+          files: [
+            'bower_components/chart.js/dist/Chart.min.js',
+            'bower_components/angular-chart.js/dist/angular-chart.min.js'
+          ]
+        }]);
+      }],
+    }
+  })
+  .state('app.main', {
+    url: '/dashboard',
+    templateUrl: 'views/main.html',
+    //page title goes here
+    ncyBreadcrumb: {
+      label: 'Home',
+    },
+    //page subtitle goes here
+    params: { subtitle: 'Welcome to ROOT powerfull Bootstrap & AngularJS UI Kit' },
+    resolve: {
+      loadPlugin: ['$ocLazyLoad', function ($ocLazyLoad) {
+        // you can lazy load files for an existing module
+        return $ocLazyLoad.load([
+          {
+            serie: true,
+            name: 'chart.js',
+            files: [
+              'bower_components/chart.js/dist/Chart.min.js',
+              'bower_components/angular-chart.js/dist/angular-chart.min.js'
+            ]
+          },
+        ]);
+      }],
+      loadMyCtrl: ['$ocLazyLoad', function($ocLazyLoad) {
+        // you can lazy load controllers
+        return $ocLazyLoad.load({
+          files: ['js/controllers/main.js']
+        });
+      }]
+    }
+  })
+
+  //components
+  .state('app.components.settings', {
+    url: '/settings',
+    templateUrl: 'views/components/settings.html',
+    ncyBreadcrumb: {
+      label: 'Settings'
+    }
+  })
+  .state('app.components.referral', {
+    url: '/referral',
+    templateUrl: 'views/components/referral.html',
+    ncyBreadcrumb: {
+      label: 'Referral'
+    }
+  })
+  .state('app.components.kyc', {
+    url: '/kyc',
+    templateUrl: 'views/components/kyc.html',
+    ncyBreadcrumb: {
+      label: 'KYC'
+    }
+  })
+  .state('app.components.transaction', {
+    url: '/transaction',
+    templateUrl: 'views/components/transaction.html',
+    ncyBreadcrumb: {
+      label: 'Transaction History'
+    }
+  })
+  .state('app.components.gettokens', {
+    url: '/gettokens',
+    templateUrl: 'views/components/gettokens.html',
+    ncyBreadcrumb: {
+      label: 'Get Tokens'
+    }
+  })
+
+  // Additional Pages
+  .state('appSimple.login', {
+    url: '/login',
+    templateUrl: 'views/pages/login.html'
+  })
+  .state('appSimple.register', {
+    url: '/register',
+    templateUrl: 'views/pages/register.html'
+  })
+  .state('appSimple.404', {
+    url: '/404',
+    templateUrl: 'views/pages/404.html'
+  })
+  .state('appSimple.500', {
+    url: '/500',
+    templateUrl: 'views/pages/500.html'
+  })
+}]);
